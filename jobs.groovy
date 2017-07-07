b496afcbbffbc24e3ad327c5750b705e5a71b9af
Asemirski <al.semirski@gmail.com>
2017-07-07 14:49:33 +0300

    scm added

--- jobs.groovy
+++ jobs.groovy
@@ -1,5 +1,8 @@
 job("EPBYMINW3093/MNTLAB-asemirski-main-build-job") {
+		scm {
+       			github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
+ 		 }
 	       	 parameters {
-     choiceParam('BRANCH_NAME', ['asemirski', 'master'], 'Choose branch')
-   }
+   			 choiceParam('BRANCH_NAME', ['asemirski', 'master'], 'Choose branch')
+  		 }
 }
