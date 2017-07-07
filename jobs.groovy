7ba16394b186559ecdfae73de7e22e48fe1c0e58
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 11:43:27 +0300

    debug of new job 2

--- jobs.groovy
+++ jobs.groovy
@@ -1,8 +1,5 @@
 //TODO:some stuff
-def testJob = freeStyleJob('DSL-Task-1-Child')
-testJob.with {
-    description 'A sample of Job'
-}
+
 testJob('DSL-Task-1-Child') {
     scm {
         github 'MNT-Lab/mntlab-dsl', vtarasiuk
