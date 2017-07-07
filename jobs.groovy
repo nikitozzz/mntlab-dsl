9d1ccbc6335c2ed388273af32b582b69ed30e83d
Forbear <silnyagindan@gmail.com>
2017-07-07 11:25:37 +0300

    init jobs.groovy file

---
+++ jobs.groovy
@@ -0,0 +1,30 @@
+freeStyleJob('MNTLAB-dsilnyagin-main-build-job'){
+    description 'Build and test the app.'
+    scm {
+      	 github 'sheehan/job-dsl-playground'
+    }
+    steps {
+      	  gradle 'test'
+    }
+    publishers {
+       	 archiveJunit 'build/test-results/**/*.xml'
+    }
+}
+['MNTLAB-dsilnyagin-child1-build-job',
+ 'MNTLAB-dsilnyagin-child2-build-job',
+ 'MNTLAB-dsilnyagin-child3-build-job',
+ 'MNTLAB-dsilnyagin-child4-build-job'
+].each { 
+    freeStyleJob(it) {
+    	description 'Build and test the app.'
+    	scm {
+       	 github 'sheehan/job-dsl-playground'
+    	}
+    	steps {
+      	  gradle 'test'
+    	}
+    	publishers {
+       	 archiveJunit 'build/test-results/**/*.xml'
+    	}
+	}
+}
