d5469852561d4f1e12744859a3b5c9786717e368
Asemirski <al.semirski@gmail.com>
2017-07-07 16:15:54 +0300

    repo branches list added

--- jobs.groovy
+++ jobs.groovy
@@ -1,3 +1,17 @@
+def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+def command = "git ls-remote -h $gitURL"
+def proc = command.execute()
+proc.waitFor()
+if ( proc.exitValue() != 0 ) { 
+  println "Error, ${proc.err.text}" 
+  System.exit(-1)
+}
+
+def repobr = proc.in.text.readLines().collect { 
+  it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
+}
+
+
 job("EPBYMINW3093/MNTLAB-asemirski-main-build-job") {
 		scm {
        			github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
@@ -5,14 +19,15 @@ job("EPBYMINW3093/MNTLAB-asemirski-main-build-job") {
 	       	 parameters {
    			 choiceParam('BRANCH_NAME', ['asemirski', 'master'], 'Choose branch')
   		 }
-		for (i = 1; i <4; i++) {
+		for (i = 1; i <2; i++) {
  			 job("MNTLAB-asemirski-child${i}-build-job") {
 				scm {
        					github 'MNT-Lab/mntlab-dsl', 'asemirski'
     				}
 				steps {
         				shell('chmod +x ./script.sh; ./script.sh')
-       				 }	
+       				 }
+				parameters {choiceParam("BRANCH_NAME", [repobr],'Choose branch')}	
 			}
 		}
 }
