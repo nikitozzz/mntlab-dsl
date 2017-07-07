c2c9af53cf51ae79d0a0f42c9c701a025841867b
Andrei Konchyts <a.konchic@gmail.com>
2017-07-07 14:23:43 +0300

    Add BRANCH_NAME to main job

--- jobs.groovy
+++ jobs.groovy
@@ -1,5 +1,11 @@
 job('./EPBYMINW3092/MNTLAB-akonchyts-main-build-job') {
    description 'This job is required to trigger the rest four from one place'
+   parameters {
+     choiceParam('BRANCH_NAME', ['akonchyts', 'master'], 'Choose appropriate branch')
+   }
+   scm {
+       github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
+   }
    steps {
      shell('echo "Main Job"')
    }
@@ -8,9 +14,6 @@ job('./EPBYMINW3092/MNTLAB-akonchyts-main-build-job') {
 ['1', '2', '3', '4'].each { suffix ->
 job('./EPBYMINW3092/MNTLAB-akonchyts-child' + suffix + '-build-job') {
     description 'The job executes the cloned ‘script.sh’ from branch propagated by main job'
-    scm {
-        github 'sheehan/job-dsl-playground'
-    }
     steps {
       shell('echo "Child Job"')
     }
