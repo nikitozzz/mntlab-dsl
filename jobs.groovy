8a6779b358e5813d247e9260353f8887a46080c8
aion3181 <aion3181@gmail.com>
2017-07-07 17:43:30 +0300

    configuring jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,34 +1,34 @@
-def project = 'MNT-Lab/mntlab-dsl'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-
 freeStyleJob('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
     description 'DSL task main job.'
     scm {
         github ''
     }
     steps {
-        g
+        shell ('echo hello world')
     }
     publishers {
         archiveJunit ''
     }
 }
 (1..4).each {
-            def jobN = it.value
-            freeStyleJob("EPBYMINW2033/MNTLAB-hpashuto-child$jobN-build-job") {
-                description "DSL task child$jobN job."
-                parameters {
-                    choiceParam ('BRANCH_NAME', branches.name )
-                }
-                scm {
-                    github ''
-                }
-                steps {
-                    gradle 'test'
-                }
-                publishers {
-                    archiveJunit ''
-                }
+    def jobN = it.value
+    freeStyleJob("EPBYMINW2033/MNTLAB-hpashuto-child$jobN-build-job") {
+        description "DSL task child$jobN job."
+        parameters {
+            gitParam('BRANCH_NAME') {
+                description('Revision commit SHA')
+                type('BRANCH')
+                defaultValue('hpashuto')
             }
         }
+        scm {
+            github ('MNT-Lab/mntlab-dsl')
+        }
+        steps {
+            gradle 'test'
+        }
+        publishers {
+            archiveJunit ''
+        }
+    }
+}
