a64957c01a03e532e8890079f9feedfedba7495f
Asemirski <al.semirski@gmail.com>
2017-07-07 14:12:53 +0300

    added choise param for main

--- jobs.groovy
+++ jobs.groovy
@@ -1,2 +1,5 @@
-freeStyleJob("EPBYMINW3093/MNTLAB-asemirski-main-build-job")
-
+job("EPBYMINW3093/MNTLAB-asemirski-main-build-job"){
+	pameters {
+       	 choiceParam('choose branch', ['master 1 (default)','asemirski'], 'here u can choise branch')
+    }
+}
