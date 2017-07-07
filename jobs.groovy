508c68942382e3af8b77df808b7ebc0315b7a090
Asemirski <al.semirski@gmail.com>
2017-07-07 13:54:34 +0300

    in folder creation feature

--- jobs.groovy
+++ jobs.groovy
@@ -1,8 +1,2 @@
-freeStyleJob("MNTLAB-asemirski-main-build-job") {
- scm {
-        git("git://github.com/quidryan/aws-sdk-test.git")
-    }
-    triggers {
-        scm('H/15 * * * *')
-    }
+freeStyleJob("EPBYMINW3093/MNTLAB-asemirski-main-build-job")
 }
