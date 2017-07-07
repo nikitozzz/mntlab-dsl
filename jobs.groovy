3697356e21702399bf015aa53d2c5b4b33c879cc
Forbear <silnyagindan@gmail.com>
2017-07-07 15:14:46 +0300

    jobs version - 9 time: 15:14:46

--- jobs.groovy
+++ jobs.groovy
@@ -27,7 +27,7 @@ freeStyleJob('EPBYMINW1374/MNTLAB-dsilnyagin-main-build-job'){
     	description 'Build and test the app.'
 	environmentVariables {
     	    //scriptFile('/var/server/config/jenkins/workspace/EPBYMINW1374/mntlab-ci-dsl/script.sh')
-	    script('cp /var/server/config/jenkins/workspace/EPBYMINW1374/mntlab-ci-dsl ./)
+	    script('cp /var/server/config/jenkins/workspace/EPBYMINW1374/mntlab-ci-dsl ./')
 	    scriptFile('script.sh')
     	}
     }
