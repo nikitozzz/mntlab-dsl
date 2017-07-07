6e563fb26ab21360f3f4cbd3381143fa3c78dbf7
Forbear <silnyagindan@gmail.com>
2017-07-07 14:17:50 +0300

    jobs version '0'

--- jobs.groovy
+++ jobs.groovy
@@ -6,6 +6,17 @@ freeStyleJob('EPBYMINW1374/MNTLAB-dsilnyagin-main-build-job'){
 	downstream('EPBYMINW1374/MNTLAB-dsilnyagin-child3-build-job', 'SUCCESS')
 	downstream('EPBYMINW1374/MNTLAB-dsilnyagin-child4-build-job', 'SUCCESS')
     }
+    parameters {
+        activeChoiceParam('CHOICE-1') {
+            description('Allows user choose from multiple choices')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["choice1", "choice2"]')
+                fallbackScript('"fallback choice"')
+            }
+        }
+    }
 }
 ['EPBYMINW1374/MNTLAB-dsilnyagin-child1-build-job',
  'EPBYMINW1374/MNTLAB-dsilnyagin-child2-build-job',
