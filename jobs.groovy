e84a6447a1e11ac36037e1fe22874be5c2049840
Zakhar Virinsky <virinsky@gmail.com>
2017-07-07 11:58:35 +0300

    check params

---
+++ jobs.groovy
@@ -0,0 +1,19 @@
+job('MNTLAB-zvirinsky-main-build-job'){
+	description 'Main Job'
+//	scm {
+//        github 'sheehan/job-dsl-playground'
+//    }
+//    triggers { 
+//       scm 'H/5 * * * *' 
+//	} 
+//    steps {
+//        gradle 'test'
+//    }
+ //   publishers {
+//        archiveJunit 'build/test-results/**/*.xml'
+//    }
+	parameters {
+        booleanParam('FLAG', true)
+        choiceParam('BRANCH_NAME', ['zvirinsky (default)', 'master'])
+    }
+}
