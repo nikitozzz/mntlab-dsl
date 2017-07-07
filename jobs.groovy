119c2a870f7deedce523ca14f7d8874d6b7dc79d
Pavel Yurchuk <paulyurchuk@gmail.com>
2017-07-07 14:23:47 +0300

    Script optimisation

--- jobs.groovy
+++ jobs.groovy
@@ -1,11 +1,6 @@
-freestylejob('EPBYMINW6405/MNTLAB-pyurchuk-main-build-job'){
-  
-  description 'Building necessary jobs'
-  
-  steps {
-        shell ('echo "The job is done! Take some beer."')
-    }
-}
+freeStyleJob('EPBYMINW6405/MNTLAB-pyurchuk-main-build-job'){
+    description 'Building necessary jobs'
+	}
 
 ['EPBYMINW6405/MNTLAB-pyurchuk-child1-build-job',
  'EPBYMINW6405/MNTLAB-pyurchuk-child2-build-job',
@@ -13,8 +8,6 @@ freestylejob('EPBYMINW6405/MNTLAB-pyurchuk-main-build-job'){
  'EPBYMINW6405/MNTLAB-pyurchuk-child4-build-job'
 ].each {
     freeStyleJob(it) {
-        steps {
-            shell ('echo "Hello there!"')
-        }
-	}
+    	description 'The job was created successfully'
+    }
 }
