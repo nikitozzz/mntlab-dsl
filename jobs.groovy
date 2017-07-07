694dc7745839fb1cbec2ca43ce3251fc837242a5
Asemirski <al.semirski@gmail.com>
2017-07-07 12:25:34 +0300

    added java line

--- jobs.groovy
+++ jobs.groovy
@@ -1,5 +1,6 @@
-job("DSL-Tutorial-1-Test") {
-    scm {
+freeStyleJob("DSL-Tutorial-1-Test") {
+jdk('Java 8')   
+ scm {
         git("git://github.com/quidryan/aws-sdk-test.git")
     }
     triggers {
