f93b1ec9ab9428953f7a5eb062c48cfa82ed9cda
Zakhar Virinsky <virinsky@gmail.com>
2017-07-07 20:41:15 +0300

    loooong build_trigger

--- jobs.groovy
+++ jobs.groovy
@@ -13,26 +13,26 @@ def branches = proc.in.text.readLines().collect {
     it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
    }
 
-def BUILDS_TRIGGER = """MNTLAB-zvirinsky-child1-build-job,
+/*def BUILDS_TRIGGER = """[MNTLAB-zvirinsky-child1-build-job,
                         MNTLAB-zvirinsky-child2-build-job,
                         MNTLAB-zvirinsky-child3-build-job,
-                        MNTLAB-zvirinsky-child4-build-job"""   
-
+                        MNTLAB-zvirinsky-child4-build-job]"""   
+*/
 job('./EPBYMINW2472/MNTLAB-zvirinsky-main-build-job'){
 	description 'Main Job'
 	scm {
         github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
     }
 
-    
+
     parameters {
         choiceParam('BRANCH_NAME', ['zvirinsky', 'master'], 'choose branch')
         activeChoiceParam('BUILDS_TRIGGER') {
             description('Allows user choose from multiple choices')
             filterable()
-            choiceType('SINGLE_SELECT')
+            choiceType('CHECKBOX')
             groovyScript {
-                script("$BUILD_TRIGGER")
+                script('["MNTLAB-zvirinsky-child1-build-job", "MNTLAB-zvirinsky-child2-build-job", "MNTLAB-zvirinsky-child3-build-job", "MNTLAB-zvirinsky-child4-build-job"]')
                 
             }
         }
