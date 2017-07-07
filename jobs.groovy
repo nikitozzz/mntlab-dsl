b03a435f0e2b4e4ab5f37957742f8f65f15fbc7c
Yuri Shchanouski <student@epbyminw2468.minsk.epam.com>
2017-07-07 14:50:05 +0300

    adding jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,31 @@
 //Yuri Shchanouski
+def git = "https://github.com/MNT-Lab/mntlab-dsl.git"
+def repo = "yshchanouski"
+job('EPBYMINW2468/MNTLAB-yshchanouski-main-build-job') {
+    scm {
+        github(git, repo)
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+	println('Hello from a Job DSL script!')        
+        shell('script.sh')
+    }
+}
+
+1.upto(4) {
+job('EPBYMINW2468/MNTLAB-yshchanouski-child${it}-build-job') {
+    scm {
+        github(git, repo)
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+	println('Hello from a Job DSL script!')        
+        shell('script.sh')
+    }
+}
+
+}
