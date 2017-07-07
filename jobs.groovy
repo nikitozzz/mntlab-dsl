530ef2a274e8956252288662c97e2bbc196b62fa
Forbear <silnyagindan@gmail.com>
2017-07-07 12:33:53 +0300

    try Job 12.09

--- jobs.groovy
+++ jobs.groovy
@@ -1,12 +1,4 @@
-//freeStyleJob('MNTLAB-dsilnyagin-main-build-job'){
-//    description 'Build and test the app.'
-//}
-['MNTLAB-dsilnyagin-child1-build-job',
- 'MNTLAB-dsilnyagin-child2-build-job',
- 'MNTLAB-dsilnyagin-child3-build-job',
- 'MNTLAB-dsilnyagin-child4-build-job'
-].each { 
-    freeStyleJob(it) {
-    	description 'Build and test the app.'
-	}
+def myJob = freeStyleJob('SimpleJob')
+myJob.with {
+    description 'A Simple Job'
 }
