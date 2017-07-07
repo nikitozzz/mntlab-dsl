ecb25c141f852a8ed748abc8a13cb56a4cce8162
Forbear <silnyagindan@gmail.com>
2017-07-07 12:09:16 +0300

    try Job 12.09

--- jobs.groovy
+++ jobs.groovy
@@ -1,6 +1,6 @@
-freeStyleJob('MNTLAB-dsilnyagin-main-build-job'){
-    description 'Build and test the app.'
-}
+//freeStyleJob('MNTLAB-dsilnyagin-main-build-job'){
+//    description 'Build and test the app.'
+//}
 ['MNTLAB-dsilnyagin-child1-build-job',
  'MNTLAB-dsilnyagin-child2-build-job',
  'MNTLAB-dsilnyagin-child3-build-job',
