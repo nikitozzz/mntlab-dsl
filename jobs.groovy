89a0ded32cec43634219e4a60fed3d58dce6aa9c
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 14:32:09 +0300

    Automated job creation.

--- jobs.groovy
+++ jobs.groovy
@@ -1,47 +1,28 @@
 def gitrepo = 'MNT-Lab/mntlab-dsl'
 def branchname = 'vtarasiuk'
-job ('EPBYMINW2471/MNTLAB-vtarasiuk-child1-build-job') {
-    scm {
-        github (gitrepo, branchname)
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    steps {
-    	shell('./script.sh')
-    }
-}
-job ('EPBYMINW2471/MNTLAB-vtarasiuk-child2-build-job') {
-    scm {
-        github (gitrepo, branchname)
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    steps {
-    	shell('./script.sh')
+
+for (i in 1 .. 4) {
+    job("EPBYMINW2471/MNTLAB-vtarasiuk-child$i-build-job") {
+        scm {
+            github(gitrepo, branchname)
+        }
+        triggers {
+            scm('H/5 * * * *')
+        }
+        steps {
+            shell('./script.sh')
+        }
     }
 }
-job ('EPBYMINW2471/MNTLAB-vtarasiuk-child3-build-job') {
+job("EPBYMINW2471/MNTLAB-vtarasiuk-main-build-job") {
     scm {
-        github (gitrepo, branchname)
+        github(gitrepo, branchname)
     }
     triggers {
         scm('H/5 * * * *')
     }
     steps {
-    	shell('./script.sh')
-    }
-}
-job ('EPBYMINW2471/MNTLAB-vtarasiuk-child4-build-job') {
-    scm {
-        github (gitrepo, branchname)
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    steps {
-    	shell('./script.sh')
+        shell('./script.sh')
     }
 }
 
