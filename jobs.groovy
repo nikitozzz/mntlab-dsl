1102b78a31c53e3c6a75fac63ef975e77dbe9f12
Andrei Konchyts <a.konchic@gmail.com>
2017-07-07 15:51:49 +0300

    Add BRANCH_NAME to child job

--- jobs.groovy
+++ jobs.groovy
@@ -1,3 +1,15 @@
+def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+def command = "git ls-remote -h $gitURL"
+def proc = command.execute()
+proc.waitFor()
+if ( proc.exitValue() != 0 ) {
+   println "Error, ${proc.err.text}"
+   System.exit(-1)
+}
+def branches = proc.in.text.readLines().collect {
+   it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
+}
+
 job('./EPBYMINW3092/MNTLAB-akonchyts-main-build-job') {
    description 'This job is required to trigger the rest four from one place'
    parameters {
@@ -7,15 +19,21 @@ job('./EPBYMINW3092/MNTLAB-akonchyts-main-build-job') {
        github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
    }
    steps {
-     shell('echo "Main Job"')
+       shell('echo "Main Job"')
    }
 }
 
 ['1', '2', '3', '4'].each { suffix ->
 job('./EPBYMINW3092/MNTLAB-akonchyts-child' + suffix + '-build-job') {
     description 'The job executes the cloned ‘script.sh’ from branch propagated by main job'
+    parameters {
+        choiceParam('BRANCH_NAME', branches)
+    }
+    scm {
+        github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
+    }
     steps {
-      shell('echo "Child Job"')
+        shell('chmod + x script.sh; ./script.sh > output.txt')
     }
 }
 }
