d831df7cc0fc09b89f2fc2a612a48b51ab93131d
artem-aksenkin <artem_askenkin@gmail.co>
2017-07-07 12:52:28 +0100

    folders for jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,41 @@
-/vagrant/dsl-test.groovy
+/**
+ * Created by student on 07.07.17.
+ */
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+
+job('EPBYMINW3088/MNTLAB-aaksionkin-DSL-build-job'){
+    description 'Build and test the app.'
+    scm {
+        github 'sheehan/job-dsl-playground'
+    }
+    steps {
+        gradle 'test'
+    }
+    publishers {
+        archiveJunit 'build/test-results/**/*.xml'
+    }
+}
+['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
+ 'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
+ 'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
+ 'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job'
+].each {
+    freeStyleJob(it) {
+        description 'Build and test the app.'
+        scm {
+            github 'sheehan/job-dsl-playground'
+        }
+        triggers {
+            scm 'H/5 * * * *'
+        }
+        steps {
+            gradle 'test'
+        }
+        publishers {
+            archiveJunit 'build/test-results/**/*.xml'
+        }
+    }
+}
