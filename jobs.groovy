8b80b6b08ee105868dee72b9235fb365faf12785
Pavel Yurchuk <paulyurchuk@gmail.com>
2017-07-07 14:12:01 +0300

    Specified machine name for each job

--- jobs.groovy
+++ jobs.groovy
@@ -1,18 +1,16 @@
-import jenkins.*
-import jenkins.model.*
-  
-job('Create MNTLAB jobs'){
+freestylejob('EPBYMINW6405/MNTLAB-pyurchuk-main-build-job'){
     scm {
         github 'https://github.com/MNT-Lab/mntlab-dsl.git'
     }
     steps {
-        shell ('echo "Hello there!"')
+        shell ('echo "The job is done! Take some beer."')
     }
 }
-['MNTLAB-pyurchuk-child1-build-job',
- 'MNTLAB-pyurchuk-child2-build-job',
- 'MNTLAB-pyurchuk-child3-build-job',
- 'MNTLAB-pyurchuk-child4-build-job'
+
+['EPBYMINW6405/MNTLAB-pyurchuk-child1-build-job',
+ 'EPBYMINW6405/MNTLAB-pyurchuk-child2-build-job',
+ 'EPBYMINW6405/MNTLAB-pyurchuk-child3-build-job',
+ 'EPBYMINW6405/MNTLAB-pyurchuk-child4-build-job'
 ].each {
     freeStyleJob(it) {
         steps {
