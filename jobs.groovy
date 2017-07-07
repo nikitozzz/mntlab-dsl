b2393c32d33f828b93087ad260aae0bd89b47d2e
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 15:07:14 +0300

    Parameters for jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,8 +1,14 @@
 def gitrepo = 'MNT-Lab/mntlab-dsl'
 def branchname = 'vtarasiuk'
+def branchApi = new URL("https://api.github.com/repos/${gitrepo}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+def BRANCH_NAME = branches.name
 
 for (i in 1 .. 4) {
     job("EPBYMINW2471/MNTLAB-vtarasiuk-child$i-build-job") {
+        parameters {
+            stringParam('MESSAGE', '-->Param-message <--!')
+        }
         scm {
             github(gitrepo, branchname)
         }
@@ -10,6 +16,7 @@ for (i in 1 .. 4) {
             scm('H/5 * * * *')
         }
         steps {
+
             shell('chmod +x ./script.sh')
             shell('./script.sh')
         }
