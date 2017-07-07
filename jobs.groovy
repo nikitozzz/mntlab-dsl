477befcbdb957097836257887993a660e67d0721
SuperBazis <bazi_s@mail.ru>
2017-07-07 16:39:44 +0300

    children was added

--- jobs.groovy
+++ jobs.groovy
@@ -10,3 +10,18 @@ job('EPBYMINW2629/MNTLAB-atsuranau-main-build-job') {
         archiveJunit 'build/test-results/**/*.xml'
     }
 }
+(1..4).each {
+	job('EPBYMINW2629/MNTLAB-atsuranau-child$it-build-job') {
+    description 'Build and test main job.'
+    scm {
+        github 'MNT-Lab/mntlab-dsl','atsuranau'
+    }
+    steps {
+        gradle 'clean test'
+    }
+    publishers {
+        archiveJunit 'build/test-results/**/*.xml'
+    }
+}
+}
+	
