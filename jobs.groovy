29b6166e0347d384109ac5c3a303094977c0ed9f
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-07 16:35:42 +0300

    run parameters edition

--- jobs.groovy
+++ jobs.groovy
@@ -23,15 +23,16 @@ job('EPBYMINW3088/MNTLAB-aaksionkin-DSL-build-job') {
                 }
             }
         }
-        runParam('EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
-                'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
-                'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
-                'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job')
+
+        //(String parameterName, String jobToRun, String description, String filter)
+
+        runParam('child1-build-job','EPBYMINW3088/MNTLAB-aksionkin-child1-build-job','1st child job', 'ALL')
+
         gitParam('BRANCH') {
             description('branch selection')
             type('BRANCH')
         }
-                ['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
+        ['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
          'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
          'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
          'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job'
