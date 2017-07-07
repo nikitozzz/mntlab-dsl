a1b3baa52041713af2ff11390db2f9a82a35411a
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 12:08:21 +0300

    Try to cast the most simple job

--- jobs.groovy
+++ jobs.groovy
@@ -1,20 +1,7 @@
 //TODO:some stuff
-def giturl = 'https://github.com/MNT-Lab/mntlab-dsl.git'
-def branchname = 'vtarasiuk'
+def jobName = 'example'
 
-folder('TestFolder')
-def testJob = freeStyleJob('TestFolder/DSL-Task-1-Child')
-
-job(testJob) {
-    scm {
-        git (giturl, branchname)
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    steps {
-        shell(readFileFromWorkspace('script.sh'))
-    }
+job(jobName) {
 }
 
 
