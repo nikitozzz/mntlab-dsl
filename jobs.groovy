d4e4081fd73fe4e031931acd96d127047c1f9f87
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 11:31:45 +0300

    test creation of new job

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,12 @@
 //TODO:some stuff
+job('DSL-Task-1-Child') {
+    scm {
+        git('git://github.com/MNT-Lab/mntlab-dsl.git', vtarasiuk)
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        shell(executeFileFromWorkspace('script.sh'))
+    }
+}
