12aaa5706791b693f84f3d5ff085d7431b551ded
aion3181 <aion3181@gmail.com>
2017-07-07 16:11:43 +0300

    configuring jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,10 +1,10 @@
-job('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
+freeStyleJob('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
     description 'DSL task main job.'
     scm {
         github ''
     }
     steps {
-        gradle 'test'
+        g
     }
     publishers {
         archiveJunit ''
@@ -12,7 +12,7 @@ job('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
 }
 (1..4).each {
             def jobN = it.value
-            job("EPBYMINW2033/MNTLAB-hpashuto-child$jobN-build-job") {
+            freeStyleJob("EPBYMINW2033/MNTLAB-hpashuto-child$jobN-build-job") {
                 description "DSL task child$jobN job."
                 scm {
                     github ''
