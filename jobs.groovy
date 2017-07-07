c05108cca54e82d174f8f59bec8ae098df57391d
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-07 16:03:28 +0300

    git param added git scm info edit

--- jobs.groovy
+++ jobs.groovy
@@ -6,41 +6,51 @@ import jenkins.model.*
 import hudson.*
 import hudson.model.*
 
-job('EPBYMINW3088/MNTLAB-aaksionkin-DSL-build-job'){
+job('EPBYMINW3088/MNTLAB-aaksionkin-DSL-build-job') {
     description 'Build and test the app.'
-    scm {
-        github 'https://github.com/MNT-Lab/mntlab-dsl.git'
-        branch('aaksionkin')
-    }
-    steps {
-        gradle 'test'
-    }
-    publishers {
-        archiveJunit 'build/test-results/**/*.xml'
-    }
-}
-['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
- 'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
- 'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
- 'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job'
-].each {
-    freeStyleJob(it) {
-        description 'Echo the shell.sh.'
+    parameters {
+        gitParam('branch') {
+            description('branch selection')
+            type('branch')
+        }
         scm {
             git {
                 remote {
                     name('origin')
                     url('https://github.com/MNT-Lab/mntlab-dsl.git')
                 }
-                branch('aaksionkin')
                 triggers {
                     scm 'H/5 * * * *'
                 }
                 steps {
                     shell(readFileFromWorkspace('script.sh'))
                 }
-
+            }
+        }
+        ['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
+         'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
+         'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
+         'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job'
+        ].each {
+            freeStyleJob(it) {
+                description 'Echo the shell.sh.'
+                scm {
+                    git {
+                        remote {
+                            name('origin')
+                            url('https://github.com/MNT-Lab/mntlab-dsl.git')
+                        }
+                        branch('aaksionkin')
+                        triggers {
+                            scm 'H/5 * * * *'
+                        }
+                        steps {
+                            shell(readFileFromWorkspace('script.sh'))
+                        }
+                    }
+                }
             }
         }
     }
 }
+
