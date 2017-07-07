91f042c161103201152e3455c58932f655b2fc10
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 18:50:57 +0300

    Main-child jobs trigger

--- jobs.groovy
+++ jobs.groovy
@@ -68,7 +68,6 @@ job("EPBYMINW2471/MNTLAB-vtarasiuk-main-build-job") {
                 condition('UNSTABLE_OR_BETTER')
                 parameters {
                     currentBuild()
-                    blockBuildUntilComplete(true)
                 }
             }
         }
