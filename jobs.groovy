820c709365fde3dee73b01ea0c162f55f97c1212
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-07 17:55:11 +0300

    git parameters edition.0.8

--- jobs.groovy
+++ jobs.groovy
@@ -16,17 +16,18 @@ job('EPBYMINW3088/MNTLAB-aaksionkin-DSL-build-job') {
         parameters {
             //choiceParam(String parameterName, List<String> options, String description)
             choiceParam('JOB_TO_RUN', ['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
-                                     'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
-                                     'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
-                                     'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job'],
+                                       'EPBYMINW3088/MNTLAB-aksionkin-child2-build-job',
+                                       'EPBYMINW3088/MNTLAB-aksionkin-child3-build-job',
+                                       'EPBYMINW3088/MNTLAB-aksionkin-child4-build-job'],
                     'Choose appropriate JOB')
-        }
 
-        gitParam('BRANCH') {
-            description('branch selection')
-            type('BRANCH')
-            //branch('')
-            defaultValue('aaksionkin') // empty by default
+
+            gitParam('BRANCH') {
+                description('branch selection')
+                type('BRANCH')
+                //branch('')
+                defaultValue('aaksionkin') // empty by default
+            }
         }
         //creating child jobs
         ['EPBYMINW3088/MNTLAB-aksionkin-child1-build-job',
