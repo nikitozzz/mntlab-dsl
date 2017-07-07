cd20b14c93a5953518af9c83a9728caa70c64d04
Forbear <silnyagindan@gmail.com>
2017-07-07 13:48:44 +0300

    jobs in Daniil's folder

--- jobs.groovy
+++ jobs.groovy
@@ -1,10 +1,10 @@
-tyleJob('Daniil Silnyagin/MNTLAB-dsilnyagin-main-build-job'){
+tyleJob('EPBYMINW1374/MNTLAB-dsilnyagin-main-build-job'){
     description 'Build and test the app.'
 }
-['Daniil Silnyagin/MNTLAB-dsilnyagin-child1-build-job',
- 'Daniil Silnyagin/MNTLAB-dsilnyagin-child2-build-job',
- 'Daniil Silnyagin/MNTLAB-dsilnyagin-child3-build-job',
- 'Daniil Silnyagin/MNTLAB-dsilnyagin-child4-build-job'
+['EPBYMINW1374/MNTLAB-dsilnyagin-child1-build-job',
+ 'EPBYMINW1374/MNTLAB-dsilnyagin-child2-build-job',
+ 'EPBYMINW1374/MNTLAB-dsilnyagin-child3-build-job',
+ 'EPBYMINW1374/MNTLAB-dsilnyagin-child4-build-job'
 ].each { 
     freeStyleJob(it) {
     	description 'Build and test the app.'
