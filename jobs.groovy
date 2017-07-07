88ea8b58acbc2cf6817ebc0aba75fe31f9aa2a00
Andrei Konchyts <a.konchic@gmail.com>
2017-07-07 13:17:00 +0300

    Create 4 simple child jobs

--- jobs.groovy
+++ jobs.groovy
@@ -1,6 +1,18 @@
 job('MNTLAB-akonchyts-main-build-job') {
    description 'This job is required to trigger the rest four from one place'
    steps {
-     shell('echo "first step"')
+     shell('echo "Main Job"')
    }
+}
+
+['1', '2', '3', '4'].each { suffix ->
+job('MNTLAB-akonchyts-child' + suffix + '-build-job') {
+    description 'The job executes the cloned ‘script.sh’ from branch propagated by main job'
+    scm {
+        github 'sheehan/job-dsl-playground'
+    }
+    steps {
+      shell('echo "Child Job"')
+    }
+}
 }
