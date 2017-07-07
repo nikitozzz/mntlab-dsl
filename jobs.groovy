5a616ea4057d574b4a504e0fed2a9870c49da466
SuperBazis <bazi_s@mail.ru>
2017-07-07 15:57:14 +0300

    absolutely changed

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,15 @@
 job('EPBYMINW2629/MNTLAB-atsuranau-main-build-job') {
-   description 'Main Job'
-scm {
-       github 'MNT-Lab/mntlab-dsl','atsuranau' 
-   }
+    description 'Build and test main job.'
+    scm {
+        github 'MNT-Lab/mntlab-dsl','atsuranau'
+    }
    triggers { 
        scm 'H/5 * * * *' 
-   } 
-   steps { 
-        gradle 'clean test' 
-   } 
-   publishers {
-       archiveJunit 'build/test-results/**/*.xml' 
-        } 
+   }
+    steps {
+        gradle 'clean test'
+    }
+    publishers {
+        archiveJunit 'build/test-results/**/*.xml'
+    }
 }
