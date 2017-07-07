c631a28b85d6c435be54f6d1b3074f13d84b7691
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-07 15:45:01 +0300

    Parameters

--- jobs.groovy
+++ jobs.groovy
@@ -1,13 +1,16 @@
 def gitrepo = 'MNT-Lab/mntlab-dsl'
 def branchname = 'vtarasiuk'
-def BRANCH_NAME = branchname
 
-def testlist = ['one', 'two']
+def branchApi = new URL("https://api.github.com/repos/${gitrepo}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def BRANCH_NAME = it.name
+}
 
 for (i in 1 .. 4) {
     job("EPBYMINW2471/MNTLAB-vtarasiuk-child$i-build-job") {
         parameters {
-            choiceParam('Branch Name', testlist)
+            choiceParam('Branch Name', BRANCH_NAME)
         }
         scm {
             github(gitrepo, branchname)
