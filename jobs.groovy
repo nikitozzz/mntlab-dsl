ee49820a8b52acf5d11e97bc831b498b667f68df
Pavel Yurchuk <paulyurchuk@gmail.com>
2017-07-07 13:28:17 +0300

    MNTLAB job-examples were added

--- jobs.groovy
+++ jobs.groovy
@@ -1,11 +1,22 @@
-job('MNT-lab-job') {
+import jenkins.*
+import jenkins.model.*
+  
+job('Create MNTLAB jobs'){
     scm {
-        git('https://github.com/MNT-Lab/mntlab-dsl.git')
-    }
-    triggers {
-        scm('H/5 * * * *')
+        github 'https://github.com/MNT-Lab/mntlab-dsl.git'
     }
     steps {
-        shell(echo "Hello there!")
+        shell ('echo "Hello there!"')
     }
+}
+['MNTLAB-pyurchuk-child1-build-job',
+ 'MNTLAB-pyurchuk-child2-build-job',
+ 'MNTLAB-pyurchuk-child3-build-job',
+ 'MNTLAB-pyurchuk-child4-build-job'
+].each {
+    freeStyleJob(it) {
+        steps {
+            shell ('echo "Hello there!"')
+        }
+	}
 }
