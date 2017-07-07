6e9a396605095ad0ea0586374e8a3fd83b10ec8b
Asemirski <al.semirski@gmail.com>
2017-07-07 14:14:46 +0300

    fixed misstype

--- jobs.groovy
+++ jobs.groovy
@@ -1,5 +1,5 @@
-job("EPBYMINW3093/MNTLAB-asemirski-main-build-job"){
-	pameters {
+job("EPBYMINW3093/MNTLAB-asemirski-main-build-job") {
+	parameters {
        	 choiceParam('choose branch', ['master 1 (default)','asemirski'], 'here u can choise branch')
     }
 }
