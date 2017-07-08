324044283870238243387994d5b21cacdcf40404
aion3181 <aion3181@gmail.com>
2017-07-08 20:46:41 +0300

    Trying again script for branchs list

--- jobs.groovy
+++ jobs.groovy
@@ -1,17 +1,22 @@
-def git1 = "MNT-Lab/mntlab-dsl"
-/**
+def gitrepo = 'MNT-Lab/mntlab-dsl'
+def branchname = 'hpashuto'
+
+/** Geting list of branches*/
 def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
 def command = "git ls-remote -h $gitURL"
+
 def proc = command.execute()
 proc.waitFor()
+
 if ( proc.exitValue() != 0 ) {
     println "Error, ${proc.err.text}"
     System.exit(-1)
 }
+
 def branches = proc.in.text.readLines().collect {
     it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
 }
-*/
+
 freeStyleJob('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
     description 'DSL task main job.'
     parameters {
@@ -26,7 +31,7 @@ freeStyleJob('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
         }
     }
     scm {
-        github (git1, '$BRANCH_NAME')
+        github (gitrepo, '$BRANCH_NAME')
     }
     steps {
         conditionalSteps {
@@ -77,11 +82,11 @@ freeStyleJob('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
     freeStyleJob("EPBYMINW2033/MNTLAB-hpashuto-child$jobN-build-job") {
         description "DSL task child$jobN job."
         parameters {
-            choiceParam("BRANCH_NAME", ['hpashuto', 'master'])
+            choiceParam("BRANCH_NAME", branches)
         }
 
         scm {
-            github(git1, '$BRANCH_NAME')
+            github(gitrepo, '$BRANCH_NAME')
         }
         steps {
             shell('chmod +x script.sh && bash -ex script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt script.sh jobs.groovy')
