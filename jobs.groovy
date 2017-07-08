5dee623e025070d9e5c8138b2308ae27b43af0cc
aion3181 <aion3181@gmail.com>
2017-07-08 14:17:26 +0300

    fixing little bugs

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,4 @@
-def git = "MNT-Lab/mntlab-dsl"
+def git1 = "MNT-Lab/mntlab-dsl"
 /**
 def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
 def command = "git ls-remote -h $gitURL"
@@ -17,33 +17,33 @@ freeStyleJob('EPBYMINW2033/MNTLAB-hpashuto-main-build-job') {
     parameters {
         choiceParam("BRANCH_NAME", ['hpashuto', 'master'])
     }
-}
     scm {
-        github (git, '$BRANCH_NAME')
+        github (git1, '$BRANCH_NAME')
     }
     steps {
         shell ('echo hello world')
     }
-
+}
 (1..4).each {
     def jobN = it.value
     freeStyleJob("EPBYMINW2033/MNTLAB-hpashuto-child$jobN-build-job") {
         description "DSL task child$jobN job."
         parameters {
-            choiceParam("BRANCH_NAME", branches)
-            }
+            choiceParam("BRANCH_NAME", ['hpashuto', 'master'])
         }
+
         scm {
-            github (git, '$BRANCH_NAME')
+            github(git1, '$BRANCH_NAME')
         }
         steps {
-            shell('bash -ex script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt script.sh jobs.groovy')
+            shell('chmod +x script.sh && bash -ex script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt script.sh jobs.groovy')
         }
         publishers {
-            archiveArtifacts{
-                pattern(output.txt)
-                pattern(${BRANCH_NAME}_dsl_script.tar.gz)
+            archiveArtifacts {
+                pattern('output.txt')
+                pattern('${BRANCH_NAME}_dsl_script.tar.gz')
                 onlyIfSuccessful()
             }
         }
     }
+}
