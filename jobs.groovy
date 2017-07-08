20ae157efa67c0531f7d645f837fc7e58c7568e5
Andrew Doropey <andry.doropey@gmail.com>
2017-07-09 00:00:29 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -3,6 +3,12 @@ def childList = []
         childList << "EPBYMINW2695/MNTLAB-adoropei-child${it}-build-job"
 }
 
+def result = ("git ls-remote -h -t https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+def branchList = result.in.text.readLines().collect
+        {
+            it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
+        }
+
 
 job('EPBYMINW2695/MNTLAB-adoropei-main-build-job') {
     description 'Build and test the app.'
@@ -31,8 +37,31 @@ job('EPBYMINW2695/MNTLAB-adoropei-main-build-job') {
     childList.each {
         job(it){
             parameters {
-        		choiceParam('BRANCH_NAME', ['adoropei', 'master'])
-   			}
+                        choiceParameter {
+                                name('BRANCH_NAME')
+                                script {
+                                         groovyScript {
+                                                script {
+                                                        script("""def result = ("git ls-remote -h -t https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+                                                                  def branches = result.in.text.readLines().collect
+                                                                  {
+                                                                    it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
+                                                                  }
+                                                                  branches  """)
+                                                        sandbox(true)
+                                                }
+                                                fallbackScript {
+                                                        script('')
+                                                        sandbox(false)
+                                                }
+                                        }
+                                }	
+                                choiceType('PT_CHECKBOX')
+                            description('Choose jobs to trigger')
+                                randomName('param-4712')
+                                filterable(false)
+                        }
+   	    }
             scm {
         		github 'MNT-Lab/mntlab-dsl','${BRANCH_NAME}'
     		}
