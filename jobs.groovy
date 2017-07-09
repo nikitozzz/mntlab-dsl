04542ab9794c4954ddea30fe0ecc5af3946fddae
alexeikorzhov <aliaksei_karzhou@epam.com>
2017-07-09 17:56:37 +0300

    Update jobs.groovy
    Change extended choice parameer

--- jobs.groovy
+++ jobs.groovy
@@ -1,39 +1,39 @@
 //Block with mane job
 job('EPBYMINW2466/MNTLAB-{akarzhou}-main-build-job') {
 // Add github scm with two branches and checkbox with child jobs
-    scm {
-        github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
+scm {
+	github 'MNT-Lab/mntlab-dsl', '$BRANCH_NAME'
 	}
-     parameters {
-     choiceParam('BRANCH_NAME', ['akarzhou', 'master'], 'Choose ich branch you want to use')
-	      extendedChoiceParameterDefinition {
-          name('Select job')
-          type('multiselect')
-	  visibleItemCount(4)
-          value('MNTLAB-akarzhou-child1-build-job,MNTLAB-akarzhou-child2-build-job')
- 	  multiSelectDelimiter(',')
+parameters {
+     	choiceParam('BRANCH_NAME', ['akarzhou', 'master'], 'Choose ich branch you want to use')
+		extendedChoiceParameterDefinition {
+          		name('BUILDS_TRIGGER')
+          		type('CHECKBOX')
+	  		visibleItemCount(4)
+          		value('MNTLAB-akarzhou-child1-build-job,MNTLAB-akarzhou-child2-build-job')
+ 	  		multiSelectDelimiter(',')
           }
 }
-	triggers {
+triggers {
 	scm ('H/3 * * * *)		
 }
-  steps {
-     shell(' echo "Publish artefact for childs jobs"')
+steps {
+	shell(' echo "Publish artefact for childs jobs"')
 }
 //Publishing artifact for chlid jobs
 publishers {
         archiveArtifacts {
-            pattern('script.sh')
-            onlyIfSuccessful()
+        	pattern('script.sh')
+            	onlyIfSuccessful()
         }
 downstreamParameterized {
-            trigger('EPBYMINW2466/MNTLAB-{akarzhou}-child1-build-job, EPBYMINW2466/MNTLAB-{akarzhou}-child2-build-job') {
-            block {
+	trigger('${BUILDS_TRIGGER}') {
+            	block {
                     buildStepFailure('FAILURE')
                     failure('FAILURE')
                     unstable('UNSTABLE')
                 }
-	    parameters {
+	    	parameters {
                     currentBuild()           
             }
      }
@@ -59,31 +59,31 @@ def branches = proc.in.text.readLines().collect {
 // Creating new 4 jobs
 ['1', '2', '3', '4'].each { suffix ->
 job('EPBYMINW2466/MNTLAB-{akarzhou}-child' + suffix + '-build-job') {
-	parameters {
+parameters {
 	choiceParam('BRANCH_NAME', branches)
 }
-	triggers {
+triggers {
 	scm ('H/3 * * * *)		
 }	
 //Copying artifact from main job
 steps {
-copyArtifacts('EPBYMINW2466/MNTLAB-{akarzhou}-main-build-job') {
-            includePatterns('script.sh')
-            targetDirectory('./')
-            flatten()
-            optional()
-            buildSelector {
-                latestSuccessful(true)
+	copyArtifacts('EPBYMINW2466/MNTLAB-{akarzhou}-main-build-job') {
+		includePatterns('script.sh')
+		targetDirectory('./')
+		flatten()
+		optional()
+		buildSelector {
+	latestSuccessful(true)
             }
-}
+	}
 //Run script, archivate output information and make artifacts
-shell('chmod +x ./script.sh && ./script.sh > output.txt && tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz output.txt script.sh')
+	shell('chmod +x ./script.sh && ./script.sh > output.txt && tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz output.txt script.sh')
 }
-      publishers {
-        archiveArtifacts {
-            pattern('output.txt')
-  	    pattern('${BRANCH_NAME}_dsl_script.tar.gz')
-            onlyIfSuccessful()
+publishers {
+	archiveArtifacts {
+		pattern('output.txt')
+		pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+		onlyIfSuccessful()
         }
     }
   
