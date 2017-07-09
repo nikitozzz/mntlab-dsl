57535f5ac050f29a551d1b97b2efddfb87444406
PaulYurchuk <paulyurchuk@gmail.com>
2017-07-09 20:33:30 +0300

    Main job changes

--- jobs.groovy
+++ jobs.groovy
@@ -16,12 +16,12 @@ def branches = proc.in.text.readLines().collect {
 }
 
 freeStyleJob("EPBYMINW6405/MNTLAB-pyurchuk-main-build-job"){
-    description 'Building necessary jobs'
+    description ('Building necessary jobs')
     
 parameters {
      choiceParam('BRANCH_NAME', ['pyurchuk', 'master'], 'Select the branch')
-        activeChoiceParam('BUILDS_STRIGGER') {
-            description('Select child job')
+        activeChoiceParam('BUILDS_TRIGGER') {
+            description('Available options')
             choiceType('CHECKBOX')
             groovyScript {
                 script('["MNTLAB-pyurchuk-child1-build-job", "MNTLAB-pyurchuk-child2-build-job", "MNTLAB-pyurchuk-child3-build-job", "MNTLAB-pyurchuk-child4-build-job"]')
@@ -35,20 +35,23 @@ scm {
 
 steps {
     downstreamParameterized {
-        trigger('$BUILDS_STRIGGER') {
+        trigger('$BUILDS_TRIGGER') {
             block {
                 buildStepFailure('FAILURE')
                 failure('FAILURE')
                 unstable('UNSTABLE')
+            }    
+            parameters{
+                currentBuild()
             }
-            parameters {
-                predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
-            }
-        }   
-
-    }
+        }
+    }   
+    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt')
 }
-}   
+publishers { 
+  archiveArtifacts('output.txt')
+}
+}
 
 1.upto(4){
 freeStyleJob("EPBYMINW6405/MNTLAB-pyurchuk-child${it}-build-job") {
