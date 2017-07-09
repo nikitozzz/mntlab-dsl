ef5d482fa0647d37de0bc8a83a5407c073471fb5
Andrew Doropey <andry.doropey@gmail.com>
2017-07-09 13:19:55 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -68,9 +68,10 @@ job('EPBYMINW2695/MNTLAB-adoropei-main-build-job') {
             steps {
                         shell( "chmod 777 script.sh" )
       			shell( "./script.sh > output.txt" )
-                        shell( ''' if [ -f "jobs.groovy" ]; then 
-                                                tar -czf $BRANCH_NAME_dsl_script.tar.gz jobs.groovy output.txt
-                                        else    tar -czf $BRANCH_NAME_dsl_script.tar.gz output.txt
+                        shell( ''' if [[ -f jobs.groovy ]]; then 
+                                              tar -czf $BRANCH_NAME_dsl_script.tar.gz jobs.groovy output.txt
+                                        else    
+                                              tar -czf $BRANCH_NAME_dsl_script.tar.gz output.txt
                                         fi ''' )
             }
             publishers {
