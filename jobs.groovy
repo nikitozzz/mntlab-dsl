76a8325b33933bfba91d666946158b8edb20754d
Anton Maslakov <antoxam1997@gmail.com>
2017-07-09 16:03:02 +0300

    Test2

--- jobs.groovy
+++ jobs.groovy
@@ -1,11 +1,24 @@
 
 job('MNTLAB-amaslakou-main-build-job'){
-
-    define "Main job"
-    scm{
-        github "MNT-Lab/mntlab-dsl", "amaslakou"
+    description 'Build and test the app.'
+    scm {
+        github 'sheehan/job-dsl-playground'
     }
     steps {
         gradle 'test'
+    }
+    (1..4).each {
+        job("EPBYMINW2695/MNTLAB-amaslakou-child${it}-build-job"){
+            scm {
+                github 'MNT-Lab/mntlab-dsl','amaslakou'
+            }
+            steps {
+                shell( "chmod 777 script.sh" )
+                shell( "./script.sh > output.txt" )
+            }
+        }
+    }
+    publishers {
+        archiveJunit 'build/test-results/**/*.xml'
     }
 }
