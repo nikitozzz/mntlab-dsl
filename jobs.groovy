da593251c1268d800e82e2283629058a872293af
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-10 11:49:08 +0300

    gitParam outlined

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,10 @@
+def command = "git ls-remote -h https://github.com/MNT-Lab/mntlab-dsl.git" as java.lang.Object
 def proc = command.execute()
+proc.waitFor()
+if ( proc.exitValue() != 0 ) {
+    println "Error, ${proc.err.text}"
+    System.exit(-1)
+}
 def branches = proc.in.text.readLines().collect {
     it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
 }
