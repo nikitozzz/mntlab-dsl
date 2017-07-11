97e93c3619bd495e1e5eb3678bbc4504ce510e54
Forbear <silnyagindan@gmail.com>
2017-07-11 12:39:15 +0300

    Commit at 12:39:15.11.of.June

--- jobs.groovy
+++ jobs.groovy
@@ -40,7 +40,9 @@ freeStyleJob('EPBYMINW1374/MNTLAB-dsilnyagin-main-build-job'){
 	choiceParam("BRANCH_NAME", mainBr)
 	activeChoiceParam('BUILD_TRIGGER') {
 	    choiceType('CHECKBOX')
-	    groovyScript { script (jobsMass) sandbox(true) }	
+	    groovyScript { script (jobsMass)
+		//sandbox(true) }
+	    }	
 	    //groovyScript { script('["EPBYMINW1374/MNTLAB-dsilnyagin-child1-build-job","EPBYMINW1374/MNTLAB-dsilnyagin-child2-build-job","EPBYMINW1374/MNTLAB-dsilnyagin-child3-build-job","EPBYMINW1374/MNTLAB-dsilnyagin-child4-build-job"]') }
 	}
     }
