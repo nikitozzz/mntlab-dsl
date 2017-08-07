6524ddd9c4cedf8c6db042f7b66e1db478a417cd
nikitozzz <ryazan3@gmail.com>
2017-08-07 06:40:32 +0000

    create branch

--- groovy_task.groovy
+++ groovy_task.groovy
@@ -29,7 +29,7 @@ job("EPRURYAW0380-MNTLAB-${student_surname}-main-build-job") {
       }
       steps {
         shell('chmod 777 ./script.sh; ./script.sh > output.txt')
-        shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh jobs.groovy' )
+        shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh groovy_task.groovy' )
       }
       publishers {
        			archiveArtifacts '${BRANCH_NAME}_dsl_script.tar.gz, output.txt'
