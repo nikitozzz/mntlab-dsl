f964fc0e0837f1d2e759fce27d91f3d5c70b1001
nikitozzz <ryazan3@gmail.com>
2017-08-07 09:32:25 +0000

    create branch

--- groovy_task.groovy
+++ groovy_task.groovy
@@ -1,58 +1,44 @@
-def student_surname = "zubkov"
-def jobsList = []
+def student = "kamyshev"
+def jobList = []
 def firstJobIndex = 1
 def lastJobIndex = 4
-def command = "git ls-remote -h -t https://github.com/nikitozzz/mntlab-dsl.git"
+def command = "git ls-remote -h https://github.com/PavelKamyshev/mntlab-dsl.git"
 
-//delete previous child jobsList
-import jenkins.model.*
-
-def matchedJobs = Jenkins.instance.items.findAll { job ->
-    job.name =~ /EPRURYAW0380*/
-}
-    
-matchedJobs.each { job ->
-    println job.name
-    job.delete()
-}
-
-job("EPRURYAW0380-MNTLAB-${student_surname}-main-build-job") 
-	{
-		def selectedBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
-		selectedBranches.removeAll {!(["master",student_surname].contains(it)) 
-	}
+job("EPRURYAW0380-MNTLAB-${student}-main-build-job") {
   
-for(i=firstJobIndex; i<lastJobIndex+1; i++)
-	{ 
-		parameters 
-			{
-				choiceParam('BRANCH_NAME', selectedBranches,'')
-				booleanParam("EPRURYAW0380-MNTLAB-${student_surname}-child${i}-build-job", true,"")
-			}
+  def selectedBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
+  selectedBranches.removeAll {!(["master",student].contains(it)) }
+  
+  for(i=firstJobIndex; i<lastJobIndex+1; i++){
+    
+    parameters {
+      choiceParam('BRANCH_NAME',  selectedBranches,'')
+      booleanParam("EPRURYAW0380-MNTLAB-${student}-child${i}-build-job", true,"")
+    }
 
     //create downstream job
-    job("EPRURYAW0380-MNTLAB-${student_surname}-child${i}-build-job") {
+    job("EPRURYAW0380-MNTLAB-${student}-child${i}-build-job") {
       scm {
-        github('nikitozzz/mntlab-dsl', student_surname)
+        github('PavelKamyshev/mntlab-dsl', student)
       }
       def allBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
-      allBranches.remove(student_surname)
-      allBranches.add(0,student_surname)
+      allBranches.remove(student)
+      allBranches.add(0,student)
       parameters {
         choiceParam('BRANCH_NAME',  allBranches,'')
       }
       steps {
         shell('chmod 777 ./script.sh; ./script.sh > output.txt')
-        shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh groovy_task.groovy' )
+        shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh jobs.groovy' )
       }
       publishers {
        			archiveArtifacts '${BRANCH_NAME}_dsl_script.tar.gz, output.txt'
      }
     }
-    jobsList << "MNTLAB-${student_surname}-child${i}-build-job"
+    jobList << "MNTLAB-${student}-child${i}-build-job"
     steps {
         downstreamParameterized {
-                trigger("EPRURYAW0380-MNTLAB-${student_surname}-child${i}-build-job") {
+                trigger("EPRURYAW0380-MNTLAB-${student}-child${i}-build-job") {
                         block{
                                 buildStepFailure('FAILURE')
                                 failure('FAILURE')
