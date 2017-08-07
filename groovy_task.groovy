1803ab8da0fac522af20d04e618e7e03b9d17381
nikitozzz <ryazan3@gmail.com>
2017-08-07 06:29:47 +0000

    create branch

---
+++ groovy_task.groovy
@@ -0,0 +1,54 @@
+def student_surname = "zubkov"
+def jobList = []
+def firstJobIndex = 1
+def lastJobIndex = 4
+def command = "git ls-remote -h https://github.com/nikitozzz/mntlab-dsl.git"
+
+job("EPRURYAW0380-MNTLAB-${student_surname}-main-build-job") {
+  
+  def selectedBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
+  selectedBranches.removeAll {!(["master",student_surname].contains(it)) }
+  
+  for(i=firstJobIndex; i<lastJobIndex+1; i++){
+    
+    parameters {
+      choiceParam('BRANCH_NAME',  selectedBranches,'')
+      booleanParam("EPRURYAW0380-MNTLAB-${student_surname}-child${i}-build-job", true,"")
+    }
+
+    //create downstream job
+    job("EPRURYAW0380-MNTLAB-${student_surname}-child${i}-build-job") {
+      scm {
+        github('nikitozzz/mntlab-dsl.git', student_surname)
+      }
+      def allBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
+      allBranches.remove(student_surname)
+      allBranches.add(0,student_surname)
+      parameters {
+        choiceParam('BRANCH_NAME',  allBranches,'')
+      }
+      steps {
+        shell('chmod 777 ./script.sh; ./script.sh > output.txt')
+        shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh jobs.groovy' )
+      }
+      publishers {
+       			archiveArtifacts '${BRANCH_NAME}_dsl_script.tar.gz, output.txt'
+     }
+    }
+    jobList << "MNTLAB-${student_surname}-child${i}-build-job"
+    steps {
+        downstreamParameterized {
+                trigger("EPRURYAW0380-MNTLAB-${student_surname}-child${i}-build-job") {
+                        block{
+                                buildStepFailure('FAILURE')
+                                failure('FAILURE')
+                                unstable('UNSTABLE')
+                        }
+                        parameters {
+                                predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                        }
+                }
+        }    
+    }
+  }
+}
